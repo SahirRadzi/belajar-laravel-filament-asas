d8f48016c36fae1efffeb5b39cf5e6c2c17b649f
SahirRadzi <sahir.radzi@gmail.com>
2025-02-06 21:52:09 +0800

    change a simple heroicon

--- app/Filament/Resources/PostcodeResource.php
+++ app/Filament/Resources/PostcodeResource.php
@@ -17,7 +17,7 @@ class PostcodeResource extends Resource
 {
     protected static ?string $model = Postcode::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-envelope';
 
     public static function form(Form $form): Form
     {
